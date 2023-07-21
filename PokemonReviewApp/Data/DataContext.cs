14d48c6a80b3558360260e38bf204bb6f074f3c4
Tran Tuan Anh <ttuananh372@gmail.com>
2023-07-21 10:30:13 +0700

    Add project files.

---
+++ PokemonReviewApp/Data/DataContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models;
+using System.Data;
+
+namespace PokemonReviewApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { set; get; }
+        public DbSet<Country> Countries { set; get; }
+        public DbSet<Owner> Owners { set; get; }
+        public DbSet<Pokemon> Pokemon { set; get; }
+        public DbSet<PokemonOwner> PokemonOwners { set; get; }
+        public DbSet<PokemonCategory> PokemonCategories { set; get; }
+        public DbSet<Review> Reviews { set; get; }
+        public DbSet<Reviewer> Reviewers { set; get; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId,pc.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p=>p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(c => c.OwnerId);
+        }
+
+    }
+}
---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int Rating { get;  set; }
+
+        public Reviewer Reviewer;
+        public Pokemon Pokemon;
+
+
+    }
+}
